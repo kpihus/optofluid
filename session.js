6394a244466fae4608ddf9695a22452ba41fb254
Kristjan Pihus <kristjan.pihus@eesti.ee>
2016-04-12 00:51:53 +0300

    test data from separate file

--- session.js
+++ session.js
@@ -4,6 +4,8 @@ var Handler = require('./data.js').Handler;
 var dao = require('./dao.js');
 var q = require('q');
 
+var testData = require('./test/testdata');
+
 
 var Worker = function (sessiondata, comSocket) {
   var self = this;
@@ -14,65 +16,6 @@ var Worker = function (sessiondata, comSocket) {
   var promises = [];
 
 
-
-  /*
-   ADD TEST DATA
-   */
-
-  var datafile = require('./mocha/data2.json');
-  var addToDb = function(item, time){
-    var conString = process.env.DATABASE_URL || 'postgres://localhost/optofluid';
-    var pg = require('pg');
-    var escape = require('pg-escape');
-    pg.connect(conString, function(err, client, doneDb){
-      if(err){
-        console.log(err); //TODO: Remove
-      }
-      var query = escape('INSERT INTO sensor ' +
-        '(ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, ch9, ch10, ch11, ch12, time) ' +
-        'values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
-        0, item[0], item[4], 0, item[1], item[5], 0, item[2], item[6], 0, item[3], item[7], time);
-      client.query(query, function(err, res){
-        doneDb();
-        if(err){
-          console.log(err)
-        }
-      })
-    });
-  };
-  var clearTable = function(table, callback){
-    var conString = process.env.DATABASE_URL || 'postgres://localhost/optofluid';
-    var pg = require('pg');
-    var escape = require('pg-escape');
-    pg.connect(conString, function(err, client, doneDb){
-      var query = escape('TRUNCATE TABLE %I', table);
-      client.query(query, function(err, res){
-        query = escape('SELECT count(*) FROM %I', table);
-        client.query(query, function(err, res){
-          doneDb();
-          return callback(err, res);
-        })
-      })
-    });
-  };
-  var addSensorData = function(){
-    clearTable('sensor',function(err){
-      var count = 0;
-      for(var i=0; i<datafile.length; i++){
-        setTimeout(function(){
-          var item = datafile[count++];
-          addToDb(item, count);
-        },1);
-
-      }
-
-    });
-
-  };
-  /*
-   END of ADD TEST DATA
-   */
-  
 
   self.startSession = function (callback) {
     dao.saveNewSession(session, function (err, res) {
@@ -87,7 +30,7 @@ var Worker = function (sessiondata, comSocket) {
         session.duration,
         session.dialflow
       );
-      addSensorData();
+      testData.addSensorData();
       callback(null, res);
     });
   };
---
+++ test/testdata.js
@@ -0,0 +1,50 @@
+var datafile = require('../mocha/data2.json');
+var addToDb = function(item, time){
+  var conString = process.env.DATABASE_URL || 'postgres://localhost/optofluid';
+  var pg = require('pg');
+  var escape = require('pg-escape');
+  pg.connect(conString, function(err, client, doneDb){
+    if(err){
+      console.log(err); //TODO: Remove
+    }
+    var query = escape('INSERT INTO sensor ' +
+      '(ch1, ch2, ch3, ch4, ch5, ch6, ch7, ch8, ch9, ch10, ch11, ch12, time) ' +
+      'values (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)',
+      0, item[0], item[4], 0, item[1], item[5], 0, item[2], item[6], 0, item[3], item[7], time);
+    client.query(query, function(err, res){
+      doneDb();
+      if(err){
+        console.log(err)
+      }
+    })
+  });
+};
+var clearTable = function(table, callback){
+  var conString = process.env.DATABASE_URL || 'postgres://localhost/optofluid';
+  var pg = require('pg');
+  var escape = require('pg-escape');
+  pg.connect(conString, function(err, client, doneDb){
+    var query = escape('TRUNCATE TABLE %I', table);
+    client.query(query, function(err, res){
+      query = escape('SELECT count(*) FROM %I', table);
+      client.query(query, function(err, res){
+        doneDb();
+        return callback(err, res);
+      })
+    })
+  });
+};
+exports.addSensorData = function(){
+  clearTable('sensor',function(err){
+    var count = 0;
+    for(var i=0; i<datafile.length; i++){
+      setTimeout(function(){
+        var item = datafile[count++];
+        addToDb(item, count);
+      },1);
+
+    }
+
+  });
+
+};
