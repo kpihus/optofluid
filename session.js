290432b22a0ff86c7c5ae2ca80032b063ee8c671
Kristjan Pihus <kristjan.pihus@eesti.ee>
2016-09-26 22:01:32 +0300

    For real, this time.

--- session.js
+++ session.js
@@ -3,16 +3,19 @@ var emitter = new EventEmitter();
 var Handler = require('./data.js').Handler;
 var dao = require('./dao.js');
 var q = require('q');
+var server = require('./app').server;
 
 var testData = require('./test/testdata');
 
 
 var Worker = function (sessiondata, comSocket) {
   var self = this;
-  handler = null;
+  var handler = null;
   var session = sessiondata;
-  started = false;
+  var started = false;
+  var dataHandler;
   var sessionStartTime;
+  var sessionEndTime;
   var promises = [];
 
 
@@ -30,13 +33,28 @@ var Worker = function (sessiondata, comSocket) {
         session.duration,
         session.dialflow
       );
+      server.log('Session started')
       testData.addSensorData(res.start);
       callback(null, res);
     });
   };
   
   self.stopSession = function(callback){
-    
+    started = false;
+    session.status = 'stopped'
+    session.end = sessionEndTime
+    clearTimeout(dataHandler)
+
+    dao.updateSession(session.sessId, session, function(err, res){
+      if(err){
+        throw new Error('Unable to end session', err)
+      }
+      if(res){
+        server.log('Session ended')
+        comSocket.emit('sessionended')
+        handler = null
+      }
+    })
   };
 
 
@@ -48,24 +66,26 @@ var Worker = function (sessiondata, comSocket) {
     self.handleData();
   };
 
-  self.stopProcessing = function(){
-    started = false;
-  };
-
   self.handleData = function () {
     fetchData(session.sessId, function(err, data){
       if(err){
         throw err;
       }
       // emitter.emit('next_data');
-      setTimeout(function(){
-        self.handleData();  
-      }, 500);
+
+      dataHandler = setTimeout(function(){
+        if (started) {
+          self.handleData();
+        } else {
+          self.stopSession()
+        }
+      }, 1000);
       
       if (data) {
         handler.addRaw(data, function (err, processed) {
           comSocket.emit('newdata', processed);
           if (processed && processed.timestamp) {
+            sessionEndTime = processed.timestamp;
             dao.saveSessionData(session.sessId, processed, function(err, res){
               if(err || !res){
                 console.log('Session data save failed', err);
